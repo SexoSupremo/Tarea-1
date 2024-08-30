37728c0934db582cd6afc5be4025e567ec2cca85
Esteban <ehequiel911@gmail.com>
2024-08-30 06:42:21 -0400

    Se agrega la Clase "Generic"

---
+++ taller_1/src/main/java/taller1/config/GenericDAO.java
@@ -0,0 +1,15 @@
+package taller1.config;
+
+import java.util.List;
+
+public interface GenericDAO<T, I> {
+    public List<T> listar();
+
+    public T obtener(I id);
+
+    public void eliminar(I id);
+
+    public T modificar(T param);
+
+    public T agregar(T param);
+}
